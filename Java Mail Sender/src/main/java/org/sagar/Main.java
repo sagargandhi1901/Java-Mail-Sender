e6b1dfd199ddb7c0d0f337a65f252f9d27f3e58c
Sagar Gandhi <sagargandhi968@gmail.com>
2023-01-15 15:38:36 +0530

    Adding email with attachment functionality

--- Java Mail Sender/src/main/java/org/sagar/Main.java
+++ Java Mail Sender/src/main/java/org/sagar/Main.java
@@ -2,20 +2,34 @@ package org.sagar;
 
 import org.sagar.email.EmailSender;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         EmailSender emailSender = new EmailSender();
         String mailTo = "khushhal1901@gmail.com";
         String mailFrom = "sagargandhi968@gmail.com";
-        String subject = "Test Email";
-        String text = "This is a test email which is being sent from java code.";
-        boolean isEmailSent = emailSender.sendEmail(mailTo, mailFrom, subject, text);
+        String subject = "Test Email with attachment";
+        String text = "This is a test email with attachment which is being sent from java code.";
+        File file = new File("D:\\Photo.jpg");
 
+        boolean isEmailSent = emailSender.sendEmailWithAttachment(mailTo, mailFrom, subject, text, file);
         if (isEmailSent) {
             System.out.println("Email sent successfully...");
         } else {
             System.out.println("There is a problem in sending email.");
         }
+
+//        boolean isEmailSent = emailSender.sendEmail(mailTo, mailFrom, subject, text);
+//        if (isEmailSent) {
+//            System.out.println("Email sent successfully...");
+//        } else {
+//            System.out.println("There is a problem in sending email.");
+//        }
+
+
     }
 }
--- Java Mail Sender/src/main/java/org/sagar/email/EmailSender.java
+++ Java Mail Sender/src/main/java/org/sagar/email/EmailSender.java
@@ -2,8 +2,11 @@ package org.sagar.email;
 
 import jakarta.mail.*;
 import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeBodyPart;
 import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
 
+import java.io.File;
 import java.util.Properties;
 
 public class EmailSender {
@@ -18,7 +21,7 @@ public class EmailSender {
         properties.put("mail.smtp.host", "smtp.gmail.com");
 
         String username = "sagargandhi968";
-        String password = "**************";
+        String password = "aicevlqfpcovgwuj ";
 
         Session session = Session.getInstance(properties, new Authenticator() {
             @Override
@@ -33,6 +36,53 @@ public class EmailSender {
             message.setFrom(new InternetAddress(mailFrom));
             message.setSubject(subject);
             message.setText(text);
+
+            Transport.send(message);
+            flag = true;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return flag;
+    }
+
+    public boolean sendEmailWithAttachment(String mailTo, String mailFrom, String subject, String text, File file) {
+        boolean flag = false;
+
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", true);
+        properties.put("mail.smtp.starttls.enable", true);
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+
+        String username = "sagargandhi968";
+        String password = "aicevlqfpcovgwuj ";
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(mailTo));
+            message.setFrom(new InternetAddress(mailFrom));
+            message.setSubject(subject);
+
+            MimeBodyPart part1 = new MimeBodyPart();
+            part1.setText(text);
+
+            MimeBodyPart part2 = new MimeBodyPart();
+            part2.attachFile(file);
+
+            MimeMultipart multipart = new MimeMultipart();
+            multipart.addBodyPart(part1);
+            multipart.addBodyPart(part2);
+
+            message.setContent(multipart);
 
             Transport.send(message);
             flag = true;
